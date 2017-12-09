e0018ce2a7c3fff121ab6046abce74daf6fbc806
Juan Martin Giménez <jm.gimenez.inf@gmail.com>
2017-12-09 13:12:12 -0300

    Primera clase

---
+++ Proyecto Cine/Cine/src/poo/cine/Pelicula.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.cine;
+
+/**
+ *
+ * @author Candelaria
+ */
+public class Pelicula {
+    
+    // TODO completar en implementación
+    
+}
